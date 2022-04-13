a0c9f89a045a3805e90afe941a09e2290e9d4ee0
drklucas <lucasnevesp3@gmail.com>
2022-04-13 10:43:42 -0300

    productItem features

---
+++ lib/components/product_item.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem(
+    this.product, {
+    Key? key
+    }) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+       return ListTile(
+         leading: CircleAvatar(
+           backgroundImage: NetworkImage(product.imageUrl),
+         ),
+         title: Text(product.name),
+         trailing: Container(
+           width: 100,
+           child: Row(children: [
+             IconButton(
+               onPressed: () {}, 
+               icon: Icon(Icons.edit)
+              ),
+              IconButton(
+               onPressed: () {}, 
+               icon: Icon(Icons.delete)
+              ),
+           ],),
+         ),
+       );
+  }
+}
--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/app_drawer.dart';
+import 'package:shop/components/product_item.dart';
 import 'package:shop/models/product_list.dart';
 
 class ProductsPage extends StatelessWidget {
@@ -18,7 +19,12 @@ class ProductsPage extends StatelessWidget {
              padding: EdgeInsets.all(8),
              child: ListView.builder(
                itemCount: products.itemsCount,
-               itemBuilder: (ctx, i) => Text(products.items[i].name),
+               itemBuilder: (ctx, i) => Column(
+                 children: [
+                   ProductItem(products.items[i]),
+                   Divider(),
+                 ],
+               ),
              ),
            ),
        );
