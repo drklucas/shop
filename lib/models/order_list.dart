d79365622a326da73acd373961dc9bd334a6b9ba
drklucas <lucasnevesp3@gmail.com>
2022-04-14 16:13:35 -0300

    order firebase connect

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop/models/cart_item.dart';
 import 'package:shop/models/order.dart';
 import 'package:shop/utils/constants.dart';
 import 'cart.dart';
@@ -16,6 +17,34 @@ class OrderList with ChangeNotifier {
 
   int get itemsCount {
     return _items.length;
+  }
+
+  Future<void> loadOrders() async {
+    _items.clear();
+
+    final response = await http.get(
+      Uri.parse('${Constants.ORDER_BASE_URL}.json')
+      );
+
+    if(response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((orderId, orderData) {
+      _items.add(Order(
+        id: orderId, 
+        date: DateTime.parse(orderData['date']),
+        total: orderData['total'], 
+        products: (orderData['products'] as List<dynamic>).map((item) {
+          return CartItem(
+            id: item['id'], 
+            productId: item['productId'], 
+            name: item['name'], 
+            quantity: item['quantity'], 
+            price: item['price']);
+        }).toList()
+        )
+        );
+    });
+    notifyListeners();
   }
   
   Future<void> addOrder(Cart cart) async {
--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -4,22 +4,42 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order_widget.dart';
 import 'package:shop/models/order_list.dart';
 
+
 class OrdersPage extends StatelessWidget {
 
-  const OrdersPage({ Key? key }) : super(key: key);
+  Future<void> _refreshOrders(BuildContext context) {
+    return Provider.of<OrderList>(context, listen: false).loadOrders();
+  }
 
-   @override
-   Widget build(BuildContext context) {
-     final OrderList orders = Provider.of(context);
-       return Scaffold(
-           appBar: AppBar(
-             title: const Text('Meus Pedidos'),
-             ),
-           drawer: AppDrawer(),
-           body: ListView.builder(
-             itemCount: orders.itemsCount,
-             itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
-           ),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meus Pedidos'),
+      ),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.error != null) {
+            return Center(
+              child: Text('Ocorreu um erro!'),
+            );
+          } else {
+            return RefreshIndicator(
+              onRefresh: () => _refreshOrders(context),
+              child: Consumer<OrderList>(
+                builder: (ctx, orders, child) => ListView.builder(
+                  itemCount: orders.itemsCount,
+                  itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+                ),
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
